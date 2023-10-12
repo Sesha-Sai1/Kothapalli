24c62454b336d171579111cbeee8cb562e656589
Sesha-Sai1 <seshasaikothapalli1350@gmail.com>
2023-10-12 17:49:19 +0530

    many changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Project from './components/Projects/Project';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>INSTAGRAM</h1>
+      <Project/>
+    </div>
+  );
+}
+
+export default App;
